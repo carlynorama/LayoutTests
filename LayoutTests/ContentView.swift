4bb070fe5985c451ce6e96fc95872cc19a5cfa52
carlynorama <carlynorama@gmail.com>
2022-08-21 07:52:38 -0700

    Initial Commit

---
+++ LayoutTests/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  LayoutTests
+//
+//  Created by Labtanza on 8/21/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LayoutTests/LayoutTestsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutTestsApp.swift
+//  LayoutTests
+//
+//  Created by Labtanza on 8/21/22.
+//
+
+import SwiftUI
+
+@main
+struct LayoutTestsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
