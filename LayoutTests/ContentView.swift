2194cb8088520e10ac125f8831cdc4b8674f23f4
carlynorama <carlynorama@gmail.com>
2022-08-21 12:38:16 -0700

    Preview AStack

--- LayoutTests/ContentView.swift
+++ LayoutTests/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
+        AStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
