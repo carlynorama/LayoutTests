e9f356516399c2ada827d344c4088f33bc82447c
carlynorama <carlynorama@gmail.com>
2022-08-21 12:38:01 -0700

    AStack w/View That Knows

--- LayoutTests/AStack.swift
+++ LayoutTests/AStack.swift
@@ -3,7 +3,7 @@
 //  LayoutTests
 //
 //  Created by carlynorama on 8/21/22.
-//  https://www.fivestars.blog/articles/adaptive-swiftui-views/
+
 //https://www.swiftbysundell.com/articles/switching-between-swiftui-hstack-vstack/
 
 extension CGSize {
@@ -12,73 +12,78 @@ extension CGSize {
     }
 }
 
-
-
 import SwiftUI
 
 struct AStack<Content:View>: View {
-    @Environment(\.horizontalSizeClass) private var size
+    //@Environment(\.horizontalSizeClass) private var size
     let horizontalAlignment: HorizontalAlignment
     let verticalAlignment: VerticalAlignment
-    let threshold:CGFloat?
     let spacing: CGFloat?
     let content:Content
-        
-        public init(
-            horizontalAlignment: HorizontalAlignment = .center,
-            verticalAlignment: VerticalAlignment = .center,
-            spacing: CGFloat? = nil,
-            @ViewBuilder _ Content: () -> Content
-        ) {
+    
+    @State var size:CGSize = .zero
+    @State var parentSize:CGSize = .zero
+    
+    var aspectRatio:CGFloat {
+        parentSize.aspectRatio
+    }
+    
+    public init(
+        horizontalAlignment: HorizontalAlignment = .center,
+        verticalAlignment: VerticalAlignment = .center,
+        spacing: CGFloat? = nil,
+        @ViewBuilder _ Content: () -> Content
+    ) {
         self.horizontalAlignment = horizontalAlignment
         self.verticalAlignment = verticalAlignment
         self.spacing = spacing
         self.content = Content()
-            self.threshold = nil
-      }
+    }
     
     var body: some View {
-        GeometryReader { proxy in
+        ViewThatKnows(size: $size, parentSize: $parentSize) {
             if #available(iOS 16.0, *) {
-                buildNewStyleContent(for:proxy)
+                buildNewStyleContent(with:aspectRatio)
             } else {
-                buildOldStyleContent(for:proxy)
+                buildOldStyleContent(with:aspectRatio)
             }
         }
+        
+        
     }
     
     @available(iOS 16.0, *)
-    @ViewBuilder func buildNewStyleContent(for proxy:GeometryProxy) -> some View {
-        let layout = (proxy.size.aspectRatio > 1) ?
+    @ViewBuilder func buildNewStyleContent(with ratio:CGFloat) -> some View {
+        let layout = (ratio > 1) ?
         AnyLayout(HStackLayout(alignment: verticalAlignment, spacing: spacing)) :
         AnyLayout(VStackLayout(alignment: horizontalAlignment, spacing: spacing))
         
         layout {
             content
         }
-        .animation(.default, value: size)
+        .animation(.default, value: parentSize)
     }
     
-    @ViewBuilder func buildOldStyleContent(for proxy:GeometryProxy) -> some View  {
+    @ViewBuilder func buildOldStyleContent(with ratio:CGFloat) -> some View  {
         ZStack {
-            if (proxy.size.aspectRatio > 1) {
-                VStack(alignment: horizontalAlignment, spacing: spacing) { content }
-            } else {
+            if (ratio > 1) {
                 HStack(alignment: verticalAlignment, spacing: spacing) { content }
+                
+            } else {
+                VStack(alignment: horizontalAlignment, spacing: spacing) { content }
             }
-        }.animation(.default, value: size)
+        }.animation(.default, value: parentSize)
     }
     
-
     
 }
 
 struct AStack_Previews: PreviewProvider {
     static var previews: some View {
         AStack() {
-            Text("Horizontal when there's lots of space")
+            Text("Horizontal when its wide")
             Text("but")
-            Text("Vertical when space is restricted")
+            Text("Vertical when its narrow")
         }
     }
 }
